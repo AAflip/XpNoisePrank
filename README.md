9d68fffb15b935a8f7bfc171b0dc8830b4e88b12
AAflip <jedidjm2008@gmail.com>
2025-05-31 00:44:48 -0700

    final update for tonight
    It is late and I am a tried

---
+++ README.md
@@ -0,0 +1,6 @@
+This project is a second attempt at creating a windows noise prank after gaining a bit more experience.
+
+To run the prank, download the startProcess.bat file and then run it
+
+To stop the prank, open task manager and find a process called `Windows Command Processor` and end it, or alternatively you can just restart the computer
+(A method to kill the process will be made eventually)
--- startProcess.bat
+++ startProcess.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/AAflip/XpNoisePrank/refs/heads/main/playSounds.bat' -OutFile playSounds.bat"
-powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/AAflip/XpNoisePrank/refs/heads/main/runInvisible.vbs' -OutFile runInvisible.vbs"
+powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/AAflip/XpNoisePrank/refs/heads/main/extraFiles/playSounds.bat' -OutFile playSounds.bat"
+powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/AAflip/XpNoisePrank/refs/heads/main/extraFiles/runInvisible.vbs' -OutFile runInvisible.vbs"
 start runInvisible.vbs
 exit
